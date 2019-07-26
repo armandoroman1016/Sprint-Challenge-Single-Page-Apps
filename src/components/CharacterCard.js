68b4974386b409444452798de6cc9a4ce98969d2
Armando Roman <armandoroman1016@gmail.com>
2019-07-26 09:00:16 -0700

    characters page complete

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,33 @@
 import React from 'react'
+import { Card ,Image} from 'semantic-ui-react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard (props) {
+  const {gender} = props
+  const {image} = props
+  const {name} = props
+  const {species} = props
+  const {status} = props
+  const {origin} = props
+  const {id} = props
+
+  console.log(gender, image, name, species, status, origin, id)
+
+  return (
+    <div>
+    <Card>
+    <Image src={image} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{name}</Card.Header>
+      <Card.Meta>
+        <span className='gender'>{gender}</span>
+      </Card.Meta>
+      <Card.Description>
+      <p>Species : {species}</p>
+      <p>Status : {status}</p>
+      <p>Origin : {origin.name}</p>
+      </Card.Description>
+    </Card.Content>
+  </Card>
+  </div>
+  )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export default function LocationsList() {
-
+    useEffect(() => {
+        axios
+        .get('https://rickandmortyapi.com/api/locations/')
+        .then(data => {
+          console.log('locations :',data)
+        })
+        .catch(error => {
+          console.log('error',error)
+        })
+      },[])
+    
+      
 }
