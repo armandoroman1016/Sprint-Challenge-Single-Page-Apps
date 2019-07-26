50f4af2848453f12d0023aece13c0b6407fe3b8a
Armando Roman <armandoroman1016@gmail.com>
2019-07-26 09:49:15 -0700

    mvp

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,33 +1,31 @@
-import React from 'react'
-import { Card ,Image} from 'semantic-ui-react'
+import React from "react";
+import { Card, Image } from "semantic-ui-react";
 
-export default function CharacterCard (props) {
-  const {gender} = props
-  const {image} = props
-  const {name} = props
-  const {species} = props
-  const {status} = props
-  const {origin} = props
-  const {id} = props
-
-  console.log(gender, image, name, species, status, origin, id)
+export default function CharacterCard(props) {
+  const { gender } = props;
+  const { image } = props;
+  const { name } = props;
+  const { species } = props;
+  const { status } = props;
+  const { origin } = props;
+  const { id } = props;
 
   return (
     <div>
-    <Card>
-    <Image src={image} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{name}</Card.Header>
-      <Card.Meta>
-        <span className='gender'>{gender}</span>
-      </Card.Meta>
-      <Card.Description>
-      <p>Species : {species}</p>
-      <p>Status : {status}</p>
-      <p>Origin : {origin.name}</p>
-      </Card.Description>
-    </Card.Content>
-  </Card>
-  </div>
-  )
+      <Card>
+        <Image src={image} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{name}</Card.Header>
+          <Card.Meta>
+            <span className="gender">{gender}</span>
+          </Card.Meta>
+          <Card.Description>
+            <p>Species : {species}</p>
+            <p>Status : {status}</p>
+            <p>Origin : {origin.name}</p>
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    </div>
+  );
 }
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Card} from 'semantic-ui-react'
 
-export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
-  return (<span>todo: location</span>
+export default function LocationCard (props) {
+  
+  const {dimension} = props
+  const {name} = props
+  const {residents} = props
+  const {type} = props
+  const {id} = props
+
+  console.log(props)
+
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>{dimension}</Card.Meta>
+        <Card.Description>
+        <p>Residents : {residents.length}</p>
+        <p>Type : {type}</p>
+        </Card.Description>
+      </Card.Content>
+    </Card>
   )
 }
